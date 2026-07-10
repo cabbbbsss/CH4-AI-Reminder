41a189292e04c88daef060b279552a0792344da3
Gusnand <gusnanda1170@gmail.com>
2026-07-10 10:03:01 +0800

    added back swipe gesture

--- eve/eve/Views/InsightView.swift
+++ eve/eve/Views/InsightView.swift
@@ -242,3 +242,15 @@ private struct InsightEditSheet: View {
     InsightView()
   }
 }
+
+// Enable native swipe-to-go-back gesture when navigation bar is hidden
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
